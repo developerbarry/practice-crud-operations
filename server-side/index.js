d635a75aa753ffe72773743c507c925217d00e6c
Developer Barry <barryallen2558@gmail.com>
2024-11-17 22:57:45 +0600

    delete user

--- server-side/index.js
+++ server-side/index.js
@@ -67,6 +67,15 @@ async function run() {
         })
 
 
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await ourUsers.deleteOne(query)
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
