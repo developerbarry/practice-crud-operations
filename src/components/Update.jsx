e8fcfb9097862cfc4193428777b83673f1669c1e
Developer Barry <barryallen2558@gmail.com>
2024-11-17 22:52:38 +0600

    updated handler added

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -4,7 +4,29 @@ const Update = () => {
     const user = useLoaderData()
     console.log(user)
 
-   
+    const handleUpdate = (event) => {
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const newUser = { name, email }
+        console.log(newUser)
+        fetch(`http://localhost:5000/users/${user?._id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if(data.modifiedCount > 0){
+                    alert("updated successfully")
+                }
+            })
+
+    }
     return (
         <div>
             <h1>{user.name} You could update your Profile!</h1>
