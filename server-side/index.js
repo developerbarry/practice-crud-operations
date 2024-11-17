0972a61525a061cb33d7f6ccc96e84ee6347a48c
Developer Barry <barryallen2558@gmail.com>
2024-11-17 22:56:16 +0600

    user create

--- server-side/index.js
+++ server-side/index.js
@@ -40,6 +40,15 @@ async function run() {
             res.send(result)
         })
 
+        app.post('/users', async (req, res) => {
+            console.log(req.body)
+            const user = req.body;
+            const result = await ourUsers.insertOne(user)
+            res.send(result)
+        
+        })
+
+        
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
